4855e6ef6b54505694714d207f49bd50048c97a9
rajivgalva <rajivgalva@hotmail.com>
2018-03-21 00:28:23 -0400

    .

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var request = require('request'); // require in request
 var initGet = {uri: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='};
+var initDrink = {uri: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='};
 var parameter = require('./app');
 
 
@@ -20,6 +21,21 @@ var apiCaller = function (urlin, cb) {
     })
   };
 
+  var drinkCaller = function (urlin, cb) {
+    //use request to make the external http call to the JSON api
+    var url = urlin.concat(parameter.drinkid);
+    console.log (url);
+    request({
+      url: url,
+      json: true
+    }, function (error, response, body) {
+  
+      if (!error && response.statusCode === 200) {
+        cb(body);// Send body/response to callback
+      }
+    })
+  };
+
   // Call the api with a call back
   var apiGet = function(cb) {
     return apiCaller(initGet.uri, cb);
@@ -27,6 +43,13 @@ var apiCaller = function (urlin, cb) {
 
   };
 
+  var apiDrink = function(cb) {
+    return drinkCaller(initDrink.uri, cb);
+  //return apiCaller(parameter);
+
+  };
+
   module.exports = {
-    apiGet: apiGet
+    apiGet: apiGet,
+    apiDrink: apiDrink
   };
--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser')
 var param ="beer";
 var did = "";
 
-
 app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
@@ -15,8 +14,8 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
-app.listen(2400, function () {
-    console.log('Example app listening on port 2400!');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
 })
 
 
@@ -36,10 +35,20 @@ app.post('/search', (req, res) => {
     res.redirect('/')
 })
 
-app.post('/drink', (req, res) => {
-    param = req.body.search;
-    module.exports.drinkid = did; 
-    res.redirect('/')
+app.post('/drinkid', (req, res) => {
+    did = req.body.drinkid;
+    module.exports.drinkid = did;
+    console.log(param)
+    res.redirect('/drink')
 })
 
+app.get('/drink', function (req, res) {
+    //res.send('Hello World!');  
+    api.apiDrink(function (data) {
+        // render to the index.jade and pass the data from api call
+        res.render('drink',{drinkresult:data});
+      });
+  })
+
 module.exports.parameter = param; 
+module.exports.drinkid = did; 
